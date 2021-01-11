7276dfa5df734ed1e6522ebb66a4035721e773b7
unknown <kaushik09909@gmail.com>
2021-01-11 13:14:09 +0530

    made changes to the server

--- server.js
+++ server.js
@@ -3,8 +3,35 @@ var fs = require('fs');
 var app = express();
 var multer = require('multer')
 var cors = require('cors');
+var debug = require('debug')('myapp:server');
+var http = require('http');
+var PORT = process.argv[2]&&process.argv[2].slice(-4)
+var port = normalizePort(process.env.PORT || PORT || '3001');
 
 app.use(cors());
+
+app.set(port);
+
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -38,6 +65,48 @@ app.get('/', function(req, res) {
   res.sendStatus(200);
 })
 
-app.listen(8000, function() {
-    console.log('App running on port 8000');
-});
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log(`server listening on ${addr.port}`);
+
+  debug('Listening on ' + bind);
+}
+
+// app.listen(8000, function() {
+//     console.log('App running on port 8000');
+// });
